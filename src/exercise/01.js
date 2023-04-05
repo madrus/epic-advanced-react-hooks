b8d61e8b5ee35947272149602987633a0764a5a3
Andre Roussakoff <madrus@gmail.com>
2023-04-05 20:22:38 +0200

    my version of extra 2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,14 @@
 
 import * as React from 'react'
 
-function Counter({initialCount = 0, step = 5}) {
-  // 🐨 replace React.useState with React.useReducer
-	const countReducer = (count, step) => count + step
-  // const [count, setCount] = React.useState(initialCount)
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+function Counter({initialCount = 0, step = 2}) {
+  const countReducer = (state, newState) => ({...state, ...newState})
 
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => changeCount(step)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
