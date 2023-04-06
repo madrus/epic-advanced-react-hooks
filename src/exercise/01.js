e8f0a9c7daaad5df273c36f26259c9dec18e67a2
Andre Roussakoff <madrus@gmail.com>
2023-04-06 06:18:44 +0200

    kcd's version of extra 4 - correction

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -7,7 +7,10 @@ function Counter({initialCount = 0, step = 2}) {
   const countReducer = (state, action) => {
     switch (action.type) {
       case 'INCREMENT':
-        return {count: state.count + action.step}
+        return {
+          ...state,
+          count: state.count + step,
+        }
       default: {
         throw new Error(`Unsupported action type: ${action.type}`)
       }
